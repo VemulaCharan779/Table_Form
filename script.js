be652521ee336d500f4b9fd0190c44ab6300f066
VemulaCharan779 <charan.v@marketsimplified.com>
2024-03-05 22:02:47 +0530

    Update Table to add data from local storage

--- script.js
+++ script.js
@@ -1,15 +1,95 @@
 let user = document.getElementById('username');
+let email = document.getElementById('useremail');
+let age = document.getElementById('userage');
+let phone = document.getElementById('usernumber');
 
-user.addEventListener('input',() =>{
-    console.log("working");
-    user.value = user.value.replace(/\[^a-zA-Z\s/g,'');
-}) ;
-    
+let submitButton = document.querySelector('.submit-button');
+let userTableBody = document.getElementById('userTableBody');
+
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+user.addEventListener('input', function() {
+    user.value = user.value.replace(/[^a-zA-Z\s]/g, '');
+});
+
+phone.addEventListener('input', function() {
+    phone.value = phone.value.replace(/[^0-9\s]/g, '');
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    updateUserTable();
+})
+
+submitButton.addEventListener("click", submitForm)
 
 
-function submitForm(e){
+function submitForm(e) {
     e.preventDefault();
-    if(user.value.length<5 ){
-        alert("user error");
+    let isValid = true;
+
+    if (user.value.length < 5) {
+        alert("Minimum 5 letters ");
+        isValid = false;
     }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailRegex.test(email.value)){
+        alert("email invalid");
+        isValid = false;
+    }
+
+  
+    const phoneNumberInput = phone.value.trim();
+    const phoneRegex = /^\d{10}$/;
+
+    if (!phoneRegex.test(phoneNumberInput)) {
+        alert("Phone number must be 10 digits");
+        phone.value = ""; 
+        phone.focus();
+        e.preventDefault(); 
+        isValid = false;
+    }
+
+    if(age.value>120){
+        alert('invalid age');
+        isValid = false;
+    }
+
+    if(isValid){
+        userDetails = {
+            name : user.value,
+            email : email.value,
+            age : age.value,
+            phone : phone.value
+        };
+
+        users.push(userDetails);
+        localStorage.setItem("users", JSON.stringify(users));
+
+        updateUserTable();
+
+        user.value= '';
+        email.value= '';
+        age.value='';
+        phone.value='';
+    }
+    
 }
+
+function updateUserTable(){
+    userTableBody.innerHTML= ' ';
+    
+    users.forEach(element => {
+        let row = document.createElement('tr');
+         row.innerHTML = `
+           <td>${element.name}</td>
+           <td>${element.age}</td>
+           <td>${element.email}</td>
+           <td>${element.phone}</td>
+         `;
+
+          userTableBody.appendChild(row);
+    });
+}
+
